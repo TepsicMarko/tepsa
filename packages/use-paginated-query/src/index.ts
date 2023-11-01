7062df1b957aac30eee29380ed5a2723d271b9c7
TepsicMarko <markotepshic@gmail.com>
2023-11-01 11:16:52 +0100

    fix: can't set query dependencies, wrong query type

--- packages/use-paginated-query/src/index.ts
+++ packages/use-paginated-query/src/index.ts
@@ -1,9 +1,7 @@
-import { UseQueryOptions, useQuery } from "@tanstack/react-query";
+import { UseQueryOptions, UseQueryResult, useQuery } from "@tanstack/react-query";
 import { Dispatch, SetStateAction, useState } from "react";
 
-export interface UsePaginatedQueryProps<T>
-  extends Omit<UseQueryOptions<T>, "queryFn" | "queryKey"> {
-  queryKey: string;
+export interface UsePaginatedQueryProps<T> extends Omit<UseQueryOptions<T>, "queryFn"> {
   queryFn: (pagination: PaginationOptions) => Promise<T>;
   initialPaginationConfig?: Partial<PaginationOptions>;
 }
@@ -18,15 +16,18 @@ export interface Pagination extends PaginationOptions {
   setPageSize: Dispatch<SetStateAction<number>>;
 }
 
-const usePaginatedQuery = <T>(props: UsePaginatedQueryProps<T>) => {
+interface UsePaginatedQueryReturn<T> {
+  query: UseQueryResult<T, Error>;
+  pagination: Pagination;
+}
+
+const usePaginatedQuery = <T>(props: UsePaginatedQueryProps<T>): UsePaginatedQueryReturn<T> => {
   const { queryKey, queryFn, initialPaginationConfig, ...queryOptions } = props;
   const [page, setPage] = useState(initialPaginationConfig?.page || 1);
-  const [pageSize, setPageSize] = useState(
-    initialPaginationConfig?.pageSize || 10
-  );
+  const [pageSize, setPageSize] = useState(initialPaginationConfig?.pageSize || 10);
 
   const query = useQuery({
-    queryKey: [queryKey, page, pageSize],
+    queryKey: [...queryKey, page, pageSize],
     queryFn: () => queryFn({ page, pageSize }),
     ...queryOptions,
   });
